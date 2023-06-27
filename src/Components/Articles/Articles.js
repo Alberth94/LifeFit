7372d8cca8a8a4dfa84773300cf1df426318b7ab
Alberth94 <108017350+Alberth94@users.noreply.github.com>
2023-06-27 20:52:45 +0300

    Add files via upload

---
+++ src/Components/Articles/Articles.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Articles.css';
+
+const Articles = () => {
+
+    return (
+        <div>
+            <h3>Work in progress</h3>
+        </div>
+    );
+}
+
+export default Articles;
---
+++ src/Components/User/UserRoute/CalculateRoute.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Button} from 'react-bootstrap';
+import './CalculateRoute.css';
+
+const CalculateRoute = (props) => {
+
+    async function calculateRoute() {
+        if (props.originRef.current.value === '' || props.destiantionRef.current.value === '') {
+          return;
+        }
+    
+        const directionsService = new window.google.maps.DirectionsService();
+        const results = await directionsService.route({
+          origin: props.originRef.current.value,
+          destination: props.destiantionRef.current.value,
+          travelMode: window.google.maps.TravelMode[props.selectedMode],
+        });
+    
+        props.setDirectionsResponse(results);
+        props.setDistance(results.routes[0].legs[0].distance.text);
+        props.setDuration(results.routes[0].legs[0].duration.text);
+      }
+
+    return (
+        <Button className='route-button small-button' variant='success' type='submit' onClick={calculateRoute}>
+            Calculate Route
+        </Button>
+    );
+}
+
+export default CalculateRoute;
